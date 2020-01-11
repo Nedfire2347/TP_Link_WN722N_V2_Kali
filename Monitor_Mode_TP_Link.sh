5c21f84055eb6028ad2596d4c098916518dbd937
Nedfire23 <15346748+Nedfire2347@users.noreply.github.com>
2020-01-11 18:15:45 +0100

    Update Monitor_Mode_TP_Link.sh

--- Monitor_Mode_TP_Link.sh
+++ Monitor_Mode_TP_Link.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
-if [ $# -eq 0 ]; then
-	echo '\n\tUsage :\n \tstart : Starting Monitor Mode \n\tstop : Disabling Monitor Mode'
-	echo '\tYou can precise your Interface : '
-	echo "\t\tex : start wlan0"
-elif [ $# -eq 2 ]; then
+if [ $# -eq 2 ]; then
 	var=$2
 else
 	var="wlan0"
 fi
 
 if [ "$1" = 'start' ]; then
+	echo "Enabling $var Monitor mode" 
 	airmon-ng check kill
 	ip link set $var down
 	iw dev $var set type monitor
 	service NetworkManager start
 
 elif [ "$1" = 'stop' ]; then
+	echo "Disabling $var Monitor mode"
 	airmon-ng check kill
 	ip link set $var down
 	iw dev $var set type managed
